bc1e81eaf3869f56a7cbe4ec9b32ad970b5c5b4d
hjaishan <hemnath.j1995@gmail.com>
2023-10-31 21:12:39 +0530

    read from env file

--- README.md
+++ README.md
@@ -6,6 +6,5 @@
 - Create,View,Delete and View deleted notes
 
 ## TODO
-- Runs in localhost, remove hardcoded username,pwd and get it from env variables
 - Works in windows, check works in Linux
 - Selector displays only 4 values eventhough increased the size
--- cmd/database/database.go
+++ cmd/database/database.go
@@ -4,11 +4,26 @@ import (
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
+	"log"
+	"os"
 )
 
-var connectionString = "postgres://postgres:root@localhost:5432/todo?sslmode=disable"
-
 func NewDB() *sqlx.DB {
+	viper.SetConfigFile(".env")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Error while reading config file %s", err)
+	}
+	usr, ok := viper.Get("userID").(string)
+	if !ok {
+		usr = os.Getenv("userID")
+	}
+	pwd, ok := viper.Get("pwd").(string)
+	if !ok {
+		pwd = os.Getenv("pwd")
+	}
+	connectionString := fmt.Sprintf("postgres://%s:%s@localhost:5432/todo?sslmode=disable", usr, pwd)
 	db, err := sqlx.Connect("postgres", connectionString)
 	if err != nil {
 		fmt.Println(err)
